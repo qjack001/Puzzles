386da7d7526973ba2af660d8ec0f7806dabb2c01
Jack Guinane <jackguinane@gmail.com>
2022-04-06 00:31:07 -0700

    Set up project build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,8 @@
-export default ({ renderers: []})
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+
+export default defineConfig({
+	site: 'https://guinane.xyz',
+	base: '/Puzzles',
+	integrations: [ vue() ],
+})
